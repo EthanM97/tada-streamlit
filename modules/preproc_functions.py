ccdf65ab9ce823ed3c9627f8c74903ae36606041
Sarah Seebach <scseebach@gmail.com>
2024-03-12 14:01:24 -0700

    refactor and adjust layout

---
+++ modules/preproc_functions.py
@@ -0,0 +1,107 @@
+
+# ------Description: This file contains functions for data preprocessing------
+
+#function to count number of duplicate rows in dataframe
+
+def count_duplicate_rows(df):
+    """
+    Count the number of duplicate rows in a DataFrame
+
+    Args:
+        df (pandas.DataFrame): The DataFrame to analyze
+
+    Returns:
+        int: The number of duplicate rows
+    """
+    return df.duplicated().sum()
+
+#function to remove duplicate rows from dataframe
+
+def remove_duplicate_rows(df):
+    """
+    Remove duplicate rows from a DataFrame
+
+    Args:
+        df (pandas.DataFrame): The DataFrame to clean
+
+    Returns:
+        pandas.DataFrame: The DataFrame with duplicate rows removed
+    """
+    df_clean = df.copy()  # Create a copy of the DataFrame
+    df_clean.drop_duplicates(inplace=True)
+    return df_clean
+
+#function to count number of missing values in dataframe
+
+def get_total_missing_values(df):
+    '''return the number of missing values in the data set'''
+    return df.isna().sum().sum()
+
+#function to count number of missing values in each column of dataframe
+
+def get_missing_values_by_feature(df):
+    '''return the number of missing values in each column of the data set'''
+    return df.isna().sum()
+
+def fill_missing_values(df, fill_method, fill_feature, fill_value):
+    '''fill missing values in the data set'''
+    if fill_method == "Fill with mean":
+        df[fill_feature] = df[fill_feature].fillna(df[fill_feature].mean())
+    elif fill_method == "Fill with median":
+        df[fill_feature] = df[fill_feature].fillna(df[fill_feature].median())
+    elif fill_method == "Fill with mode":
+        df[fill_feature] = df[fill_feature].fillna(df[fill_feature].mode()[0])
+    elif fill_method == "Fill with custom value":
+        df[fill_feature] = df[fill_feature].fillna(fill_value)
+    return df
+
+#--------------------------------OUTLIER HANDLING--------------------------------
+
+#function to count number of outliers in dataframe feature
+
+def count_outliers(df, feature, threshold):
+    '''return the number of outliers in the data set'''
+    z_scores = (df[feature] - df[feature].mean()) / df[feature].std()
+    return (z_scores > threshold).sum()
+
+#function to remove outliers from dataframe feature
+
+def remove_outliers(df, feature, threshold):
+    '''remove outliers from the data set'''
+    z_scores = (df[feature] - df[feature].mean()) / df[feature].std()
+    df_clean = df.copy()  # Create a copy of the DataFrame
+    df_clean = df_clean[z_scores <= threshold]
+    return df_clean
+
+#function to copy rows with outliers to a new dataframe
+
+def get_outliers(df, feature, threshold):
+    '''return the rows with outliers in the data set'''
+    z_scores = (df[feature] - df[feature].mean()) / df[feature].std()
+    return df[z_scores > threshold]
+
+#function that returns the location of the outliers in the dataframe, their value and their z-scores
+#as a table for display
+
+def get_outliers_table(df, feature, threshold):
+    '''return the location of the outliers in the data set, their value and their z-scores'''
+    z_scores = (df[feature] - df[feature].mean()) / df[feature].std()
+    outliers = df[z_scores > threshold]
+    outliers_table = outliers.assign(z_score=z_scores[z_scores > threshold])
+    return outliers_table
+
+#get all numerical features in the dataframe
+
+def get_numerical_features(df):
+    '''return the numerical features in the data set'''
+    return df.select_dtypes(include=[np.number]).columns.tolist()   
+
+#get all categorical features in the dataframe
+
+def get_categorical_features(df):
+    '''return the categorical features in the data set'''
+    return df.select_dtypes(include=['object']).columns.tolist()
+
+
+
+
---
+++ pages/1-STEP1-UPLOAD.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import pandas as pd
+from modules.shared_functions import *
+
+#function for file upload
+        
+def secure_file_uploader():
+    """
+    Creates a secure file uploader for Streamlit apps.
+    Allows only CSV, XLSX, and XLSM files with a size under 200MB.
+
+    Returns:
+        The uploaded file as a Pandas DataFrame, or None if no valid file was uploaded.
+    """
+
+    uploaded_file = st.file_uploader(
+        "Choose a CSV, XLSX, or XLSM file",
+        type=["csv", "xlsx", "xlsm"],
+        help="Only CSV, XLSX and XLSM files are allowed"
+    )
+
+    if uploaded_file is not None:
+        # Check file size
+        if uploaded_file.size > 200 * 1024 * 1024:  # 200 MB limit
+            st.error("File size exceeds the 200MB limit")
+            return None
+
+        # Try reading the file as a DataFrame
+        try:
+            df = pd.read_csv(uploaded_file)  # Assuming it's a CSV for now
+            if uploaded_file.type == "xlsx" or uploaded_file.type == "xlsm":
+                df = pd.read_excel(uploaded_file)  # Use read_excel for Excel files
+
+            st.success("File uploaded successfully!")
+            return df
+
+        except Exception as e:
+            st.error(f"An error occurred while reading the file: {e}")
+            return None
+
+#--------------------------------PAGE SET UP--------------------------------
+
+st.set_page_config(page_title="Upload Your Data", page_icon="📈", layout="wide")
+
+st.header("Upload Your Data")
+st.write("Upload your data to get started. When you are done. Click Next to move to the next step.")
+
+#--Sidebar setup--------------------------------
+
+st.sidebar.header("Rules for Data Upload")
+st.sidebar.divider()
+st.sidebar.write("1. Only CSV, XLSX, and XLSM files are allowed.")
+st.sidebar.write("The maximum file size allowed is 200MB.")
+st.sidebar.divider()
+st.sidebar.write("*Disclaimer - Tada does not store your data. Once you close the app, your data is deleted. Tada is also not responsible for the security of your data*")
+st.sidebar.divider()
+
+
+#--File Upload Section--------------------------------
+
+uploaded_df = secure_file_uploader()
+if uploaded_df is not None:
+    if 'main_df' not in st.session_state:
+        st.session_state.main_df = uploaded_df.copy()  # Initialize session state
+        st.dataframe(st.session_state.main_df)
+
+#--Next Button--------------------------------
+        
+    col1, col2 = st.columns([18, 1])
+
+    with col1:
+        st.write("")
+    with col2:    
+        next_button = st.button("Next", key="next_button", help="Move to next step")
+        if next_button: 
+            if 'main_df' not in st.session_state:
+                st.session_state.main_df = uploaded_df.copy()  # Initialize session state
+            switch_page("STEP1-Preprocessing")
+
+
